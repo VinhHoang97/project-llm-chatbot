b9ef7e4dd494ecad30e47406ee62fcab71c38e86
hungit <nlhhung.fullstack@gmail.com>
2024-09-30 19:00:42 +0700

    change to load json document

--- backend/app.py
+++ backend/app.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import logging
 
-from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.document_loaders import JSONLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_ollama import OllamaEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
 
 class InputDataFormat(BaseModel):
     text: str
@@ -13,20 +16,30 @@ app = FastAPI()
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
+    
 
 @app.post("/process")
 def process(input: InputDataFormat):
-    loader = WebBaseLoader("https://lilianweng.github.io/posts/2023-06-23-agent/")
+    file_path='formatData/data.json'
+    loader = JSONLoader(
+        file_path=file_path,
+        jq_schema=".[].content",
+        text_content=False)
+
     data = loader.load()
 
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
     all_splits = text_splitter.split_documents(data)
+    MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
+    hf_embeddings = HuggingFaceEmbeddings(model_name=MODEL_NAME)
 
-    print("test")
-    print(all_splits[0])
+    vectorstore = Chroma.from_documents(all_splits, hf_embeddings, persist_directory="db")
     
-    return {"result": f"${input} hello"}
+    question = "Các nhân vật tiêu biểu văn hóa?"
+    docs = vectorstore.similarity_search(question)
+    # print(len(docs))
+    
+    return {"result": docs}
 
 if __name__ == "__main__":
     import uvicorn
