6f4694c7d124711b88ee55f29f59c9f1514c1b8e
hungit <nlhhung.fullstack@gmail.com>
2024-10-01 21:19:09 +0700

    Add code to query

--- backend/app.py
+++ backend/app.py
@@ -3,9 +3,11 @@ from pydantic import BaseModel
 import logging
 
 from langchain_community.document_loaders import JSONLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.llms import Ollama
+from langchain import PromptTemplate, LLMChain
 
 
 class InputDataFormat(BaseModel):
@@ -43,9 +45,34 @@ def process(input: InputDataFormat):
     chroma_store = Chroma(persist_directory=persist_directory, embedding_function=hf_embeddings)
 
     query = input.model_dump()["query"]
-    query = "tin tức nổi bật nhất hôm nay?"
-    results = chroma_store.similarity_search(query)
-    return {"result": results[0].page_content}
+    document = chroma_store.similarity_search(query)
+    
+    ollama = Ollama(base_url="http://localhost:11434", model="llama2")
+    
+    template = """Dưới đây là các tài lệu liên quan đến câu hỏi của bạn:
+        TÀI LỆU: {document}
+        Trả lời câu hỏi sau: {question}
+        
+        Hướng dẫn:
+        - Câu trả lời đầy đủ và chi tiết.
+        - Không tự tạo đáp án nếu không thể trả lời
+        - Không sử dụng các cụm từ dẫn đến một văn bản khác như "theo tài liệu, theo đường dẫn, theo thông tin,..." và các cụm từ tương tự.
+        - Đường dẫn của tài liệu chứa câu trả lời
+        - Trả lời theo format:
+        # Câu trả lời:
+        ...
+        # Tham khảo:
+        ... 
+        """
+    
+    prompt_template = PromptTemplate(
+        template=template, input_variables=["document", "question"]
+    )
+    
+    llm_chain = LLMChain(llm=ollama, prompt=prompt_template)
+    response = llm_chain.run({"document": document, "question": query})
+
+    return {"result": response}
 
 if __name__ == "__main__":
     import uvicorn
