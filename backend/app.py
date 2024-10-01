8fb84b0a7d07f74c7e4bcf60c5874307ac832e46
hungit <nlhhung.fullstack@gmail.com>
2024-10-01 20:19:42 +0700

    Add function save chroma vector embedding

--- backend/app.py
+++ backend/app.py
@@ -4,14 +4,12 @@ import logging
 
 from langchain_community.document_loaders import JSONLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores.chroma import Chroma
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
-MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
-hf_embeddings = HuggingFaceEmbeddings(model_name=MODEL_NAME)
 
 class InputDataFormat(BaseModel):
-    text: str
+    query: str
     
 app = FastAPI()
 
@@ -22,28 +20,32 @@ logger = logging.getLogger(__name__)
 
 @app.post("/create-document")
 def create_embedding():
+    MODEL_NAME = "keepitreal/vietnamese-sbert"
+    hf_embeddings = HuggingFaceEmbeddings(model_name=MODEL_NAME)
+    persist_directory = 'db'
+    
     file_path='formatData/data.json'
     loader = JSONLoader(
         file_path=file_path,
         jq_schema=".[].content",
         text_content=False)
     data = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=200)
     all_splits = text_splitter.split_documents(data)
-    vectorstore = Chroma.from_documents(all_splits, hf_embeddings, persist_directory="./chroma_langchain_db")
+    vectorstore = Chroma.from_documents(documents=all_splits, embedding=hf_embeddings, persist_directory=persist_directory)
     vectorstore.persist()
-    
-    print("test vectorstore", vectorstore)
 
 @app.post("/process")
-def process(query: str):
-    chroma_store = Chroma(
-        embedding_function=hf_embeddings, 
-        persist_directory="./chroma_langchain_db"
-    )
+def process(input: InputDataFormat):
+    MODEL_NAME = "keepitreal/vietnamese-sbert"
+    hf_embeddings = HuggingFaceEmbeddings(model_name=MODEL_NAME)
+    persist_directory = 'db'
+    chroma_store = Chroma(persist_directory=persist_directory, embedding_function=hf_embeddings)
+
+    query = input.model_dump()["query"]
     query = "tin tức nổi bật nhất hôm nay?"
     results = chroma_store.similarity_search(query)
-    return {"result": results[0]}
+    return {"result": results[0].page_content}
 
 if __name__ == "__main__":
     import uvicorn
--- frontend/app.py
+++ frontend/app.py
@@ -5,7 +5,9 @@ if st.button("Tạo không gian embedding"):
     response = requests.post("http://fastapi:8000/create-document")
     st.write("Hoàn thành")
 
+text_input = st.text_area("Nhập câu hỏi:")
+
 if st.button("Xử lý"):
-    response = requests.post("http://fastapi:8000/process", json={"text": "hello"})
+    response = requests.post("http://fastapi:8000/process", json={"query": text_input})
     result = response.json()
     st.write(result)
