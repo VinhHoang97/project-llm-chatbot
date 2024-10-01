1f3f29fdb74428a056b3ef60acd53250f11589c0
hungit <nlhhung.fullstack@gmail.com>
2024-10-01 18:23:36 +0700

    add button create embedding

--- frontend/app.py
+++ frontend/app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import requests
 
+if st.button("Tạo không gian embedding"):
+    response = requests.post("http://fastapi:8000/create-document")
+    st.write("Hoàn thành")
+
 if st.button("Xử lý"):
     response = requests.post("http://fastapi:8000/process", json={"text": "hello"})
     result = response.json()
