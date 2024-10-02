59ad6c23ff8c240a28f73f3110e7d2528001c3fb
hungit <nlhhung.fullstack@gmail.com>
2024-10-02 11:17:08 +0700

    fix bug no model in ollama container

--- backend/app.py
+++ backend/app.py
@@ -6,9 +6,9 @@ from langchain_community.document_loaders import JSONLoader
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.llms import Ollama
-from langchain import PromptTemplate, LLMChain
-
+from langchain_community.chat_models import ChatOllama
+from langchain_core.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
 
 class InputDataFormat(BaseModel):
     query: str
@@ -32,7 +32,7 @@ def create_embedding():
         jq_schema=".[].content",
         text_content=False)
     data = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=200)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=300, chunk_overlap=50, separators=["\n\n", "\n"])
     all_splits = text_splitter.split_documents(data)
     vectorstore = Chroma.from_documents(documents=all_splits, embedding=hf_embeddings, persist_directory=persist_directory)
     vectorstore.persist()
@@ -43,12 +43,11 @@ def process(input: InputDataFormat):
     hf_embeddings = HuggingFaceEmbeddings(model_name=MODEL_NAME)
     persist_directory = 'db'
     chroma_store = Chroma(persist_directory=persist_directory, embedding_function=hf_embeddings)
+    # Tạo retriever từ Chroma
+    retriever = chroma_store.as_retriever(search_type="similarity", search_kwargs={"k": 10})
 
     query = input.model_dump()["query"]
-    document = chroma_store.similarity_search(query)
-    
-    ollama = Ollama(base_url="http://localhost:11434", model="llama2")
-    
+        
     template = """Dưới đây là các tài lệu liên quan đến câu hỏi của bạn:
         TÀI LỆU: {document}
         Trả lời câu hỏi sau: {question}
@@ -63,16 +62,40 @@ def process(input: InputDataFormat):
         ...
         # Tham khảo:
         ... 
-        """
+    #     """
     
     prompt_template = PromptTemplate(
         template=template, input_variables=["document", "question"]
     )
     
-    llm_chain = LLMChain(llm=ollama, prompt=prompt_template)
-    response = llm_chain.run({"document": document, "question": query})
+    # Khởi tạo ChatOllama
+    llm = ChatOllama(
+        temperature=0,
+        base_url="http://ollama:11434/",
+        model="llama3.2:1b",
+        streaming=True,
+        top_k=10,  # Độ đa dạng câu trả lời
+        top_p=0.3,  # Mức độ tập trung của văn bản sinh ra
+        num_ctx=3072,  # Kích thước cửa sổ ngữ cảnh
+    )
 
-    return {"result": response}
+    # Sử dụng RunnableSequence để kết hợp prompt và llm
+    llm_chain = RetrievalQA(
+        llm=llm,
+        retriever=retriever,
+        prompt=prompt_template,
+        return_source_documents=True
+    )
+
+    # Gọi response từ query
+    response = llm_chain({"query": query})
+
+    # Trả kết quả
+    documents = "\n".join([doc.page_content for doc in response["source_documents"]])
+    return {
+        "result": response["result"],
+        "documents": documents
+    }
 
 if __name__ == "__main__":
     import uvicorn
